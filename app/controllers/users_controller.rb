bbab236c20d137a37935829189f13ab4609c70b9
Robert Himler <roberthimler@mac.com>
2012-07-27 05:43:58 -0400

    Deployinggggg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @users }
+      # format.json { render json: @users }
     end
   end
 
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @user }
+      # format.json { render json: @user }
     end
   end
 
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @user }
+      # format.json { render json: @user }
     end
   end
 
