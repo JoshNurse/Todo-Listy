567c2bd3af11108beafeb4276177d91518f6239e
Robert Himler <roberthimler@mac.com>
2012-08-29 12:48:05 +1200

    Fixed a bunch of security holes.

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,12 +1,12 @@
 class ListItemsController < ApplicationController
   def create
-    @list = List.find(params[:list_id])
+    @list = current_user.lists.find(params[:list_id])
     @item = @list.list_items.create(params[:list_item])
     redirect_to list_path(@list)
   end
   
   def destroy
-    @list = List.find(params[:list_id])
+    @list = current_user.lists.find(params[:list_id])
     @item = @list.list_items.find(params[:id])
     @item.destroy
     redirect_to list_path(@list)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,79 +1,66 @@
 class UsersController < ApplicationController
   
   # GET /users
-  # GET /users.json
   def index
     @users = User.count
 
     respond_to do |format|
       format.html # index.html.erb
-      # format.json { render json: @users }
     end
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    @user = current_user
 
     respond_to do |format|
       format.html # show.html.erb
-      # format.json { render json: @user }
     end
   end
 
   # GET /users/new
-  # GET /users/new.json
   def new
     @user = User.new
 
     respond_to do |format|
       format.html # new.html.erb
-      # format.json { render json: @user }
     end
   end
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(params[:user])
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to lists_path, notice: 'Your account has been successfully created!' }
-        format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /users/1
-  # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
+    @user = current_user
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: 'Your account has been updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
+    @user = current_user
     @user.destroy
 
     respond_to do |format|
@@ -81,7 +68,6 @@ class UsersController < ApplicationController
         redirect_to root_path,
         notice: "We're sorry to see you go :("
       }
-      format.json { head :no_content }
     end
   end
 end
